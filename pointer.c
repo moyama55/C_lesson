bc69eece98ba96830109686196dd211ba145563a
moyama55 <moyama04@gmail.com>
2022-04-07 10:16:20 +0900

    commit

---
+++ pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printIntByAddress(int*);
+
+int main(void){
+    int a = 1;
+    printIntByAddress(&a);
+    return 0;
+}
+
+void printIntByAddress(int* b){
+    printf("addrss is : %d",b);
+}
--- pointerLesson.c
+++ pointerLesson.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void sub(char ages[3]);
-
-int main(void){
-    char a[] = {1,2,3};
-    char b[3];
-    sub(&a[0]);
-    memcpy(b, a, 3);
-    sub(&b[0]);
-    char* c = malloc(12);
-    memcpy(&c, &b, 12);
-    if(memcmp(a, b, 3) == 0){
-        printf("right copy");
+void sub(char* addr){
+    for(int i =0; i < 3; i++){
+        printf("%d:%d\n",i + 1, *(addr + i));
     }
-    return 0;
 }
 
-void sub(char* ages){
-    for(int i = 0; i < 3; i++){
-        printf("%dsecond:%d\n", i+1,ages[i]);
+int main(void){
+    char a[] = {1, 2, 3};
+    char b[3];
+    sub(&a);
+    memcpy(b, a, 3);
+    sub(&b);
+    if(memcmp(a, b, 3) == 0){
+        printf("Critical copy\n");
     }
+
+    return 0;
 }
